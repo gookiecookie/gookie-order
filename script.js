93e05e8371393b2d98ccaa10df9da422792353c9
gookiecookie <heygookie@gmail.com>
2026-06-25 11:04:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-let cart = [];
-let total = 0;
+let items = {
+  choco: { name: "Double Chocolate", price: 8, qty: 0 },
+  smores: { name: "S’mores Gookie", price: 9, qty: 0 },
+  tiramisu: { name: "Tiramisu Lava", price: 10, qty: 0 }
+};
 
-function addItem(name, price) {
-  cart.push({ name, price });
-  total += price;
+function changeQty(key, amount) {
+  items[key].qty += amount;
+
+  if (items[key].qty < 0) items[key].qty = 0;
+
+  document.getElementById(`qty-${key}`).innerText = items[key].qty;
+
   renderCart();
 }
 
 function renderCart() {
-  let container = document.getElementById("cart-items");
-  container.innerHTML = "";
+  let cartDiv = document.getElementById("cartItems");
+  let total = 0;
 
-  cart.forEach(item => {
-    container.innerHTML += `
-      <p>${item.name} - RM${item.price}</p>
-    `;
+  cartDiv.innerHTML = "";
+
+  Object.keys(items).forEach(key => {
+    let item = items[key];
+
+    if (item.qty > 0) {
+      total += item.qty * item.price;
+
+      let div = document.createElement("div");
+      div.innerHTML = `
+        <p>${item.name} x ${item.qty} - RM ${item.qty * item.price}</p>
+      `;
+      cartDiv.appendChild(div);
+    }
   });
 
   document.getElementById("total").innerText = total;
 }
 
 function checkout() {
-  let message = "Hi Gookie! I want to order:%0A%0A";
+  let msg = "Hi Gookie! I want to order:%0A%0A";
+  let total = 0;
 
-  cart.forEach(item => {
-    message += `- ${item.name} RM${item.price}%0A`;
+  Object.keys(items).forEach(key => {
+    let item = items[key];
+
+    if (item.qty > 0) {
+      msg += `- ${item.name} x${item.qty} RM${item.qty * item.price}%0A`;
+      total += item.qty * item.price;
+    }
   });
 
-  message += `%0ATotal: RM${total}%0A%0AName:%0APickup/Delivery:%0ADate:`;
+  msg += `%0ATotal: RM${total}`;
 
-  let phone = "60102810487"; // nanti kita tukar nombor awak
-  window.open(`https://wa.me/${phone}?text=${message}`, "_blank");
+  window.open(`https://wa.me/60123456789?text=${msg}`, "_blank");
 }
